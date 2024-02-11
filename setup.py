52976db7d77b2c1fd772e40def88547634c15d3e
ptbrodie <ptbrodie@gmail.com>
2024-02-11 07:54:10 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(name='curls',
+      version='0.0.1',
+      description='A command-line alternative to Postman.',
+      url='http://github.com/ptbrodie/curls',
+      author='Patrick Brodie',
+      author_email='ptbrodie@gmail.com',
+      install_requires=[
+	  'certifi==2024.2.2',
+	  'charset-normalizer==3.3.2',
+	  'dateparser==1.2.0',
+	  'idna==3.6',
+	  'peewee==3.17.1',
+	  'python-dateutil==2.8.2',
+	  'pytz==2024.1',
+	  'regex==2023.12.25',
+	  'requests==2.31.0',
+	  'six==1.16.0',
+	  'terminaltables==3.1.10',
+	  'tzlocal==5.2',
+	  'urllib3==1.26.18'
+      ],
+      scripts=['./curls'])
